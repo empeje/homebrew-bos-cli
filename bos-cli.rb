d6acc8dbbca838c406a0b6df557ba585c2d5abd3
empeje <11813607+empeje@users.noreply.github.com>
2023-06-25 10:47:38 +0200

    Initial setup

---
+++ bos-cli.rb
@@ -0,0 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class BosCli < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/bos-cli-rs/bos-cli-rs/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "189fbfb425a3a1e825bd5fc36ea3c6910aa5786ac6ebb1a404d308efaf94035d"
+  license ""
+
+  depends_on "rust" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test bos-cli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
