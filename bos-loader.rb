989597bb02125cd43cc18037a7961b3a99a45730
empeje <11813607+empeje@users.noreply.github.com>
2023-06-25 11:19:56 +0200

    Add bos loader

---
+++ bos-loader.rb
@@ -0,0 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class BosLoader < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/near/bos-loader/archive/refs/tags/v0.7.0.tar.gz"
+  sha256 "70db83053ff43fd12c5caaafaf54ff225bdb95ad980cea15f4082deb943f44bd"
+  license ""
+
+  depends_on "rust" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test bos-loader`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
